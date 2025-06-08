603a865520ab5b8b514fffc703502c0aa60df4c6
Seamus-C <63012394+Rennik2@users.noreply.github.com>
2025-06-08 15:06:04 -0700

    update level images

--- Scripts/ball_controller.gd
+++ Scripts/ball_controller.gd
@@ -4,11 +4,14 @@ var was_hit = false
 
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if Input.is_action_just_pressed("hit"):
-		linear_velocity = normal.normalized() * -10
+		linear_velocity = normal.normalized() * (-7.0 - (SceneData.score / 5))
 		was_hit = true
 		print("click")
 
 func _on_body_entered(body: Node) -> void:
+	#if body.name == "DeathBox":
+		#print(body.name)
+	
 	if was_hit:
 		SceneData.score += 1
 		was_hit = false 
