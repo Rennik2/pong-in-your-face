8e4dfab6bdbc28c27def9838a5f7bba5ee00aff3
Seamus-C <63012394+Rennik2@users.noreply.github.com>
2025-06-08 15:24:28 -0700

    make score reset between sceens

--- Scenes/in_game_menu.gd
+++ Scenes/in_game_menu.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 func _on_return_to_main_menu_pressed() -> void:
+	SceneData.score = 0 
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
 func _on_restart_pressed() -> void:
