fff3c547a119f57060aec8a37a3a9d0a125f1f57
Seamus-C <63012394+Rennik2@users.noreply.github.com>
2025-06-07 23:35:25 -0700

    make the ball bouncy
    why did that take so long

--- add_volocity_to_ball.gd
+++ add_volocity_to_ball.gd
@@ -1,16 +1,14 @@
 extends RigidBody3D
+ 
 
-var previousVelocity = Vector3(0,0,0)
+var previous_velocity = Vector3(0,0,0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	previousVelocity = linear_velocity
+	previous_velocity = linear_velocity
 
 
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if Input.is_action_just_pressed("hit"):
-		apply_force(Vector3(0,0,-1000))
-
-
-func _on_body_entered(body: Node) -> void:
-	print(body.name)
+		linear_velocity = normal.normalized() * -3
+		print("click")
