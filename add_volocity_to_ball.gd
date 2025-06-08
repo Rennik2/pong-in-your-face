dfc3ea41bbf81c30904279a12b5351674cb2d909
Seamus-C <63012394+Rennik2@users.noreply.github.com>
2025-06-07 23:44:27 -0700

    clean up

--- add_volocity_to_ball.gd
+++ add_volocity_to_ball.gd
@@ -1,14 +1,6 @@
 extends RigidBody3D
- 
-
-var previous_velocity = Vector3(0,0,0)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	previous_velocity = linear_velocity
-
 
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if Input.is_action_just_pressed("hit"):
-		linear_velocity = normal.normalized() * -3
+		linear_velocity = normal.normalized() * -10
 		print("click")
